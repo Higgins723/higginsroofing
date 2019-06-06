6f328fa82e3e6b41671cf4afb135e71bac491275
Joshua-Higgins <joshua.higgins@rakuten.com>
2019-06-05 20:07:13 -0600

    added loading to login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,7 @@ const Login = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [hasError, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleSuccess = (response) => {
     console.log(response.data);
@@ -18,6 +19,7 @@ const Login = (props) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
     axios.post('https://higginsroofingapi.herokuapp.com/api/auth/jwt/', {
       username: email,
       password: password
@@ -27,20 +29,32 @@ const Login = (props) => {
       })
       .catch(error => {
         handleError(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }
 
   return (
     <div className="text-center login">
-      <form onSubmit={onSubmit} className="form-signin">
-        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label className="sr-only">Username</label>
-        <input onChange={(e) => setEmail(e.target.value)} value={email} id="inputEmail" className="form-control" placeholder="Username" required />
-        <label className="sr-only">Password</label>
-        <input onChange={(e) => {setPassword(e.target.value); setError(false)}} value={password} type="password" id="inputPassword" className={"form-control " + (hasError ? "is-invalid" : "")} placeholder="Password" required />
-        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p className="mt-5 mb-3 text-muted">&copy; Higgins Roofing</p>
-      </form>
+      {loading ? (
+        <div className="spinner-grow" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
+        <div>
+          {hasError && <p className="text-danger">Username and password are incorrect</p>}
+          <form onSubmit={onSubmit} className="form-signin">
+            <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <label className="sr-only">Username</label>
+            <input onChange={(e) => setEmail(e.target.value)} value={email} id="inputEmail" className="form-control" placeholder="Username" required />
+            <label className="sr-only">Password</label>
+            <input onChange={(e) => {setPassword(e.target.value); setError(false)}} value={password} type="password" id="inputPassword" className={"form-control " + (hasError ? "is-invalid" : "")} placeholder="Password" required />
+            <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            <p className="mt-5 mb-3 text-muted">&copy; Higgins Roofing</p>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
